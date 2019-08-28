6eb00b23a6901bcda8f0212b7a1be11bebe359be
baron <augmentum@163.com>
2019-08-28 11:39:52 +0800

    dd

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 DBENGINE = 'mysql'  # ENGINE OPTIONS: mysql, sqlite3, postgresql
 DBNAME = 'ershoufang'
 DBUSER = 'root'
-DBPASSWORD = ''
+DBPASSWORD = 'password'
 DBHOST = '127.0.0.1'
 DBPORT = 3306
-CITY = 'bj'  # only one, shanghai=sh shenzhen=sh......
-REGIONLIST = [u'dongcheng']  # only pinyin support
+CITY = 'hz'  # only one, shanghai=sh shenzhen=sh......
+REGIONLIST = [u'xihu']  # only pinyin support
